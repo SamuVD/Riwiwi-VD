021f12efb34e8f368504f6e547ac139de40018d1
SamuVD <samuvv2905@gmail.com>
2024-03-04 08:12:58 -0500

    cambio 1

--- Python/Mis Prácticas/Listas.py
+++ Python/Mis Prácticas/Listas.py
@@ -3,4 +3,7 @@ La información que se debe tener de los candidatos es:
 nombre, experiencia laboral en años, lenguajes de programación que domina, 
 idiomas...Las funciones que debe tener el código son: Agregar candidatos,
 Eliminar candidatos, Modificar candidatos, Mostrar candidatos.  QUE COMIENCE EL JUEGO!! """
+name = 'Yisus'
+print(f'Hola {name}') 
 
+print('xd')

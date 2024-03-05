1213f23fd06335a7531e4c2f03882eb3d6af1107
SamuVD <samuvv2905@gmail.com>
2024-03-05 04:24:52 -0500

    Más noticas

---
+++ Python/Mis Prácticas/notas_python.py
@@ -0,0 +1,64 @@
+""" 
+Operadores Aritméticos 
+-Son utilizados para resolver operaciónes matemáticas.
+SON LOS SIGUIENTES:
+. +
+. -
+. *
+. / : División 
+. // : División sin decimal
+. ** : Exponente (elevado a) 
+. % : Módulo (residuo de la división) 
+ Prioridad de los Operadores Aritméticos:
+1. ( ) 
+2. **
+3. *,/,%
+4. +,-
+
+Operadores Relacionales
+-Se utilizan para establecer entre 2 valores.
+-Compara estos valores entre si y esta comparación produce un resultado de certeza o falsedad (verdadero o falso).
+-Tienen el mismo nivel de prioridad en su evaluación.
+-Los operadores relacionales tienen menor prioridad que los operadores aritméticos.
+SON LOS SIGUIENTES: 
+- > : Mayor que 
+- < : Menor que 
+- >= : Mayor o igual que 
+- <= : Menor o igual que
+- != : Diferente 
+- == : Igual 
+Nota: Los operadores relacionales se pueden combinar con los operadores aritméticos.
+
+Operadores Lógicos 
+-Permiten construir expresiones lógicas, se obtiene como resultado un dato booleano.
+SON LOS SIGUIENTES: 
+- and que es una conjunción.
+(and es una multiplicación lógica)
+- or que es una disyunción. 
+(or es una suma lógica)
+- not solo es una negación.
+(true = false, false = true)
+Prioridad de los Operadores Lógicos
+1. not 
+2. and 
+3. or 
+Nota: Los operadores lógicos se pueden combinar con los operadores relacionales. 
+PRIORIDAD GENERAL DE LOS 3 OPERADORES:
+1. ( )
+2. **
+3. *, /, %, not
+4. +, -, and
+5. >, <, ==, >=, <=, !=, or """
+'''----------------------------------------'''
+'''
+.replace = Quita los espacios y reemplaza las cadenas.
+.append = agrega a una lista
+.capitalize = convierte a mayúsculas
+.remove = elimina elementos.
+.lower = Convierte en minúsculas.
+pass = Indica que no hay código, que sí va haber, pero por el momento no vamos a escribir nada. Esto nos ayuda a que podamos seguir ejecutando demas líneas. 
+Funciones = Bloques de código reutilizables. Dentro de esos bloques se pueden guardar parámetros.
+
+Si...= Condicional 
+Calcular = Definir función 
+'''

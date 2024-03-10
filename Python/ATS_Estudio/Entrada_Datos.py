925777e6601f021d492e3ab35e1105392e502557
SamuVD <samuvv2905@gmail.com>
2024-03-10 16:17:32 -0500

    entrada D

---
+++ Python/ATS_Estudio/Entrada_Datos.py
@@ -0,0 +1,6 @@
+#Entrada de datos
+
+apellido = input('\nDame tu primer apellido: ')
+nacimiento = (input('\nDame tu fecha de nacimiento: '))
+
+print(f'\nHola señor {apellido}su fecha de nacimiento es {nacimiento}')

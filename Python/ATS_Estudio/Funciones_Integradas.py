966f34ecc7913c06042f483f8a93eae0362e8390
SamuVD <samuvv2905@gmail.com>
2024-03-10 16:41:06 -0500

    F integradas

---
+++ Python/ATS_Estudio/Funciones_Integradas.py
@@ -0,0 +1,22 @@
+#Funciones integradas 
+
+conversion = str(29.2005)
+print(conversion)
+'\n'
+binario = bin(29)
+print(binario)
+'\n'
+hexadecimal = hex(29)
+print(hexadecimal)
+'\n'
+entero = int('0x1d',16)
+print(entero)
+'\n'
+valorAbsolut = abs(-29)
+print(valorAbsolut)
+'\n'
+redondeaNum = round(9.6)
+print(redondeaNum)
+'\n'
+NumCaract = len('Samuel')
+print(NumCaract)

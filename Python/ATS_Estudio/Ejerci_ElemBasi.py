16ae452d401bfd2a6a23eaba2d5a27689cbc3b8a
SamuVD <samuvv2905@gmail.com>
2024-03-10 19:00:32 -0500

    Ejerci d elemnBasicos

---
+++ Python/ATS_Estudio/Ejerci_ElemBasi.py
@@ -0,0 +1,40 @@
+#Ejercicios sobre Elementos básicos
+
+'''
+Ejercicio 1: Escribir la siguiente expresión en forma de expresión algorítmica
+'''
+# valorA = float(input('\nDame el valor de A: '))
+# valorB = float(input('\nDame el valor de B: '))
+# valorC = float(input('\nDame el valor de C: '))
+# totalV = (valorA**3 * (valorB**2 - 2*valorA*valorC)) // (2*valorB)
+# '\n'
+# print(totalV)
+
+'''
+Ejercicio 2: Determinar la solución lógica de la siguiente operación
+'''
+# a = float(input('\nEscriba el valor que va tener A: '))
+# b = float(input('\nEscriba el valor que va tener B: '))
+# '\n'
+# result = ((3+5*8)<3 and (-6/3*4)+2<2) or (a>b)
+# print(result)
+
+'''
+Ejercicio 3: Hacer un programa para intercambiar el valor de 2 variables
+'''
+# o = input('Pon cualquier objeto -> ')
+# n = input('Pon un ser de la naturaleza -> ')
+# '\n'
+# o , n = n , o
+# print(f'\nEl nuevo valor de O es {o}')
+# print(f'\nEl nuevo valor de N es {n}')
+
+'''
+Ejercicio 4: Hacer un programa para ingresar el radio de un círculo
+ y se reporte su área y la longitud de la circunferecia.
+'''
+# import math
+# r = float(input('\nDigite el valor del radio: '))
+# area = math.pi * r**2 
+# longi = 2 * math.pi * r
+# print(f'El área -> {area:.2f} y su longitud -> {longi:.2f}')
